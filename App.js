5dc479c97aedecfdda92440458b545e06e8f7eef
ratspin <6210110013@psu.ac.th>
2021-07-12 11:07:05 +0700

    comit 2

--- App.js
+++ App.js
@@ -1,22 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Weather from './components/weather';
+
 export default function App() {
- const doIt = () => {
- console.log("Hello from console")
- }
- return (
- <View style={styles.container}>
- <Text onPress={doIt}>Hello world</Text>
- <StatusBar style="auto" />
- </View>
- );
+  return (
+    <View style={styles.container}>
+      <Weather zipCode="90110"/>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
+
 const styles = StyleSheet.create({
- container: {
- flex: 1,
- backgroundColor: '#fff',
- alignItems: 'center',
- justifyContent: 'center',
- },
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 });
---
+++ components/Weather.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text } from 'react-native';
+export default function Weather(props){
+    return(
+        <Text>{props.zipCode}</Text>
+
+    );
+
+
+
+}
