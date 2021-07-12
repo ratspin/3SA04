9f9e859a9bbb0c76018c44752cbec4661a31156b
ratspin <6210110013@psu.ac.th>
2021-07-12 23:41:39 +0700

    commit 4.2

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ import Weather from './components/weather';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Weather zipCode="7777"/>
+      <Weather zipCode="8877707"/>
       <StatusBar style="auto" />
     </View>
   );
