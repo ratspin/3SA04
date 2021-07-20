1cdbb3cb4cae9ed084f8492b92b486d1b00a665b
ratspin <6210110013@psu.ac.th>
2021-07-20 16:10:14 +0700

    Update styles ZipCodeScreen

--- Screen/ZipCodeScreen.js
+++ Screen/ZipCodeScreen.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { FlatList, View, Text, StyleSheet } from 'react-native'
+import { FlatList, View, Text, StyleSheet ,Image} from 'react-native'
 import { TouchableHighlight } from 'react-native-gesture-handler';
 import { useNavigation } from '@react-navigation/native';
 
@@ -9,14 +9,25 @@ const availableZipItems = [
     { place: 'Chiangmai', code: '50000' }, 
     { place: 'Khonkaen', code: '40000' }, 
     { place: 'Chonburi', code: '20000' },
+    { place: 'Kathu, Phuket', code: '83120' },
+    { place: 'Bangkok', code: '10100' },
+    { place: 'Chumphon', code: '86000' },
+    { place: 'Phayao', code: '56000' },
+    { place: 'Phatthalung', code: '93000' },
+    { place: 'Satun', code: '91000' },
+    { place: 'BuriRam', code: '31000' },
+    { place: 'Ranong', code: '85000' }
 ]
 
 const ZipItem = ({place, code, navigation}) => (
+    
     <TouchableHighlight onPress={() => 
     {navigation.navigate('Weather', {zipCode: code})}}>
-        <View style ={styles.zipItem}>
-            <Text>{place}</Text>
-            <Text>{code}</Text>
+        <View style={styles.Text, styles.background}>
+            <View style={styles.zipItem}>
+                    <Text style={{ fontSize: 27, color: 'white' }}>{place}</Text>
+                    <Text style={{ fontSize: 20, color: 'white' }}>{code}</Text>
+            </View>
         </View>
     </TouchableHighlight>
 )
@@ -35,14 +46,29 @@ export default function ZipCodeScreen(){
 
 const styles = StyleSheet.create({
     zipItem: {
-        flex: 1,
+        alignItems: 'center', 
+        paddingTop: 10, 
+        paddingBottom: 10,
+    },
+    Text: {
         flexDirection: 'row',
-        justifyContent: 'space-between'
-    },
-    zipPlace: {
+        justifyContent: 'center',
+        alignItems: 'center',
         flex: 1,
+        width: '100%',
+        height: '10%'
     },
-    zipCode: {
-        flex: 1,
-    }
+    background: {
+        backgroundColor: '#FF6666',
+        marginLeft: 20,
+        marginRight: 20,
+        margin: 3,
+        borderRadius: 50,
+    },
+    container: { 
+        flex: 1, 
+        alignItems: 'center', 
+        width: '100%', 
+        height: '100%' 
+    },
 }) 
